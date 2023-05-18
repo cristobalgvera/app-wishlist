656f9ade48f51ea193da3e8535d1f049fd50ce7b
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-05-18 03:01:21 -0400

    feat(welcome): remove text alignment to last line

--- src/app/WelcomeMessage.tsx
+++ src/app/WelcomeMessage.tsx
@@ -1,10 +1,10 @@
 export function WelcomeMessage() {
   return (
-    <div className="prose prose-lg lg:prose-lg prose-p:[text-align-last:center] max-w-full">
+    <div className="prose prose-lg lg:prose-lg max-w-full">
       <h1 className="text-center grid grid-cols-6 items-center">
-        <span className="">🥳</span>
+        <span>🥳</span>
         <span className="col-span-4 max-sm:text-4xl">¡Nuestra Wish List!</span>
-        <span className="">🎉</span>
+        <span>🎉</span>
       </h1>
       <p className="mt-0">
         En esta wishlist encontrarás algunas ideas de regalos en caso de que
